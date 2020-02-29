28ebd49192b2f139cd8c2a45dc1c0de907c62aff
Auzo <akjha96@github.com>
2020-02-29 19:53:29 +0530

    Updates

---
+++ precache-manifest.8075398a944def8845fa67f64358e0d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8619ae743441897010be26dbf159b0a",
+    "url": "/Monster-rolodex/index.html"
+  },
+  {
+    "revision": "766184771ba8bedaaf42",
+    "url": "/Monster-rolodex/static/css/main.b01ac34c.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/Monster-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monster-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766184771ba8bedaaf42",
+    "url": "/Monster-rolodex/static/js/main.28cee2a4.chunk.js"
+  },
+  {
+    "revision": "fa478ba7d08fc5476b9b",
+    "url": "/Monster-rolodex/static/js/runtime-main.3fef68e8.js"
+  }
+]);
